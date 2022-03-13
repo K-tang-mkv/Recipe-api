e846108638d1636b5515ff6507fab4d3c1143500
fuck <xtang2828@gmail.com>
2022-03-13 11:26:10 +0800

    refactor routers

--- main.go
+++ main.go
@@ -18,33 +18,12 @@
 package main
 
 import (
-	"github.com/gin-contrib/sessions"
-	redisStore "github.com/gin-contrib/sessions/redis"
-	"github.com/gin-gonic/gin"
-
-	handlers "github.com/recipes-api/handlers"
+	"github.com/recipes-api/routers"
 )
 
 func main() {
-	router := gin.Default()
 
-	store, _ := redisStore.NewStore(10, "tcp", "localhost:6379", "", []byte("secret"))
-	router.Use(sessions.Sessions("recipes_api", store))
-
-	router.GET("/recipes", handlers.RecipesHandler.ListRecipesHandler)
-
-	router.POST("/signin", handlers.AuthHandler.SignInHandler)
-	router.POST("/refresh", handlers.AuthHandler.RefreshHandler)
-	router.POST("/signout", handlers.AuthHandler.SignOutHandler)
-
-	authorized := router.Group("/")
-	authorized.Use(handlers.AuthHandler.AuthMiddleware())
-	{
-		authorized.POST("/recipes", handlers.RecipesHandler.NewRecipeHandler)
-		authorized.PUT("/recipes/:id", handlers.RecipesHandler.UpdateRecipeHandler)
-		authorized.DELETE("/recipes/:id", handlers.RecipesHandler.DeleteRecipeHandler)
-		authorized.GET("/recipes/:id", handlers.RecipesHandler.GetOneRecipeHandler)
-	}
+	router := routers.InitRouter()
 
 	router.Run()
 }
